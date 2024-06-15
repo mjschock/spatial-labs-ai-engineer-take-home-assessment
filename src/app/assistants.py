0554dcef7dad2f398ac600d798fb61328173cdbc
Michael James Schock <m@mjschock.com>
2024-06-14 18:57:29 -0700

    move playback_audio tool to post_run_hook

--- src/app/assistants.py
+++ src/app/assistants.py
@@ -1,4 +1,4 @@
-from marvin.beta.assistants import Assistant
+from marvin.beta.assistants import Assistant, Thread, Run
 
 from app.tools import get_products, playback_audio
 
@@ -9,11 +9,27 @@ If a user asks a question that is not in the catalog, you should let them know t
 Always playback audio of your response to the user after outputting the text. Use the playback_audio tool to do this.
 """
 
-customer_assistant = Assistant(
+class CustomerAssistant(Assistant):
+    def pre_run_hook(self):
+        pass
+
+    def post_run_hook(self, run: "Run"):
+        thread_id = run.thread.id
+
+        thread = Thread(id=thread_id)
+        messages = thread.get_messages()
+        messages
+
+        last_message = messages[-1]
+        last_message_content_text_value = last_message.content[-1].text.value
+
+        playback_audio(last_message_content_text_value, voice='shimmer')
+
+customer_assistant = CustomerAssistant(
     name="Customer Assistant",
     instructions=instructions,
     tools=[
         get_products,
-        playback_audio,
+        # playback_audio,
     ],
 )
